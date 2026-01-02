99964ce67cded87b0430e17e1206ff373953ed8b
zakari776 <hazzouzakaria32@gmail.com>
2026-01-02 19:03:26 +0100

    1

---
+++ src/syntaxe.cpp
@@ -0,0 +1,48 @@
+#include <iostream> 
+#include <cmath>						
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <chrono>
+#define Pi 3.14
+using namespace std;
+								// include area with # + include + < name of library >
+								// include and define => keyworld 
+								// not ; need , no statament here
+
+
+int main()						//entry point of code 			
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	int x = 7;           // intilaze of variable x , type integer take integer value 
+	if (x > 5)
+	{
+		cout << "in step 1" << endl;
+		return 0;
+		cout << " not , no run even all this bloack but after rutrun  ";
+	}
+	if (x > 0)
+	{
+		cout << "in step 2" << endl;
+		return 0;
+		cout << " no run even all this bloack and after rutrun";
+	}
+
+}// retrun of any func mean end of execut it }
